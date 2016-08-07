9d7de0c30acef28175e4271af2a52d8d4bb30ae5
Brennan Hoeting <brennan@infuse.us>
2016-08-06 23:37:33 -0400

    add bg

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Show me your moves!</title>
+  <style>
+    html {
+      background: url(https://images3.alphacoders.com/643/643616.png) no-repeat center center fixed;
+      background-size: cover;
+    }
+  </style>
 </head>
 <body>
-    <h1>Captain Falcon Kiosk!</h1>
+  <h1>Captain Falcon Kiosk!</h1>
 </body>
 </html>
